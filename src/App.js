a08ee085e61a5a2594254de26d57d4f4f36c1e0f
Hannah <hannahrwatkins@gmail.com>
2023-07-28 10:28:34 -0400

    added css, implemented sign in form in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { auth } from './firebase';
 import './App.css';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      // Sign-in successful - redirect to another page
+    } catch (error) {
+      console.error('Error signing in:', error);
+      // Handle sign-in error - show message
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sign In</h1>
+      <form onSubmit={handleSignIn}>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+        <button type="submit">Sign In</button>
+      </form>
     </div>
   );
 }
 
 export default App;
+
--- src/firebase.js
+++ src/firebase.js
@@ -18,3 +18,6 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 export default firebase;
+
+
+
